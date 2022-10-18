13562da7d2628abb09593adae1f3f714d447efa0
Elliott Sánchez León <raptordroid15@gmail.com>
2022-10-17 23:59:42 -0500

    Primero

---
+++ js/app.js
@@ -0,0 +1,23 @@
+//Variables para referenciar a objetos deldocuemnto
+const carrito = document.querySelector('#carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+
+let listadoCarrito = [];
+
+const agregarCurso = (e) => {
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')){
+        const curso = e.target.parentElement.parentElement;
+        const infoCurso = {
+            imagen: curso.querySelector('img').src,
+            nombre: curso.querySelector('h4').textContent,
+            precio: curso.querySelector('p.precio').textContent,
+            id: curso.querySelector('.agregar-carrito').getAttribute('data-id'),
+            cantidad: 1
+        }
+        
+        agregarCarrito(infoCurso);
+    }
+}
